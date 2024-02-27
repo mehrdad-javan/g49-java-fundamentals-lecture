fbaa64c8e1996b5751debaef65602d8063ab2954
Mehrdad Javan <mehrdad.javan@lexicon.se>
2024-02-27 09:23:09 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,18 @@
+package se.lexicon;
+
+// re-format code: ctrl + alt - L
+
+public class App {
+
+    // single line comment
+
+    /*
+        This is a multi-line comment
+        It can be multiple lines
+    */
+
+    public static void main(String[] args) {
+        // sout + tab
+        System.out.println("Hello World!");
+    }
+}
