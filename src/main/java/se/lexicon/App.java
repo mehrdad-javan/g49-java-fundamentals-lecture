ed7cb758745abeebf2cf9537c713d0c4497af39b
Mehrdad Javan <mehrdad.javan@lexicon.se>
2024-02-27 09:51:23 +0100

    java fundamentals (variables)

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -14,5 +14,34 @@ public class App {
     public static void main(String[] args) {
         // sout + tab
         System.out.println("Hello World!");
+
+
+        short age = 25;
+        short employeeExperience = 10;
+
+        int productPrice = 5000;
+
+        long balance = 500000000000000L;
+        long totalPopulation = 7000000000L;
+
+        char grade = 'A';
+        // char grade = 'B';
+
+        boolean isStudent = true;
+        boolean isActive = false;
+
+        double amount = 50000000.45454545;
+        float radius = 5.5f;
+        double area = 5.9;
+
+        String firstName = "John";
+        String lastName = "Doe";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+
+        int a = 10, b = 8, c = 10;
+
+
     }
 }
